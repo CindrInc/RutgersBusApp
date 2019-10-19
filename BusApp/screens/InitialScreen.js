4de13884f46f4d2445f71e16472b7ca44a7cf89d
Tarun Boddupalli <tarunbod@gmail.com>
2019-10-19 18:48:11 -0400

    Initial screen design done

--- BusApp/screens/InitialScreen.js
+++ BusApp/screens/InitialScreen.js
@@ -11,27 +11,65 @@ import {
   View,
 } from 'react-native';
 
-export default function InitialScreen() {
+function InitialScreen() {
     return (
         <View style={styles.container}>
-            <View>
-                <Text style={styles.headerText}>Hello, World!</Text>
+            <View style={styles.headerContainer}>
+                <Text style={styles.headerText}>Welcome!</Text>
             </View>
-            <Image source={require('../assets/images/rutgers.png')}/>
+            <View>
+                <Image source={require('../assets/images/rutgers.png')} style={styles.rutgersImage}/>
+                <Text style={styles.imageSubtitle}>Bus Scheduler</Text>
+            </View>
+            <TouchableOpacity onPress={this.onGetStarted} style={styles.getStartedButton}>
+                <Text style={styles.getStartedButtonText}>Get Started</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
+InitialScreen.prototype.onGetStarted = function() {
+    console.log("Hello, World!");
+}
+
+export default InitialScreen;
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: Colors.scarlet
+        backgroundColor: Colors.scarlet,
+        display: 'flex',
+        alignItems: 'center',
+    },
+    headerContainer: {
+        marginBottom: 40
     },
     headerText: {
         marginTop: 50,
-        textAlign: 'center',
         fontWeight: 'bold',
+        fontSize: 48,
+        color: 'white'
+    },
+    rutgersImage: {
+        width: 304 * 0.7,
+        height: 270 * 0.7,
+        marginBottom: 10
+    },
+    imageSubtitle: {
+        textAlign: 'center',
         color: 'white',
-        fontSize: 48
+        fontSize: 24
+    },
+    getStartedButton: {
+        marginTop: 100,
+        borderWidth: 3,
+        borderStyle: 'solid',
+        borderColor: 'white',
+        borderRadius: 10,
+        padding: 10
+    },
+    getStartedButtonText: {
+        color: 'white',
+        fontSize: 36
     }
 });
